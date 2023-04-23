e7ee66e60c9ba8f02bef16febc00180799443945
Joe <jguss45@gmail.com>
2023-04-23 22:28:36 +0200

    experience section

--- src/assets/index.js
+++ src/assets/index.js
@@ -25,6 +25,10 @@ import meta from "./company/meta.png";
 import shopify from "./company/shopify.png";
 import starbucks from "./company/starbucks.png";
 import tesla from "./company/tesla.png";
+import purdue from "./company/purdue.png";
+import habit from "./company/habit.png";
+import hnm from "./company/h&m.png";
+import ecom from "./company/ecom.png";
 
 import carrent from "./carrent.png";
 import jobit from "./jobit.png";
@@ -56,6 +60,10 @@ export {
   shopify,
   starbucks,
   tesla,
+  purdue,
+  habit,
+  hnm,
+  ecom,
   carrent,
   jobit,
   tripguide,
